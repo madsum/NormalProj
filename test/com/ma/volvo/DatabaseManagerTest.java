c3bd6df7dfad6bb7a4ac07f04dc0496544985d32
Masum Islam <muhammed.islam@ericsson.com>
2019-08-08 07:05:14 +0200

    Add mockito stuff

--- test/com/ma/volvo/DatabaseManagerTest.java
+++ test/com/ma/volvo/DatabaseManagerTest.java
@@ -4,16 +4,35 @@ import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
 
 public class DatabaseManagerTest {
 
+	@Mock
+	private static DatabaseManager databaseManager;
+	static ArrayList<String> returnList = new ArrayList<String>();
+	static String pno12 = "aaa";
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		databaseManager = mock(DatabaseManager.class);
+		returnList.add(pno12);
 	}
 
 	@Test
 	public void testGetDataByPno12() {
-		fail("Not yet implemented");
+		when(databaseManager.getDataByPno12(pno12)).thenReturn(returnList);
+		DatabaseManager db = new DatabaseManager();
+		ArrayList<String> expected = databaseManager.getDataByPno12(pno12);
+		ArrayList<String> actual = new ArrayList<String>();
+		actual.add(pno12);
+		assertThat(actual, is(expected));
+		System.out.println(expected.get(0));
 	}
 
 }
